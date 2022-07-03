28e842737a015b48257e74153f3272856f6c5107
Abiola Olamilekan <abiolawalter2015@gmail.com>
2022-07-03 18:59:51 +0100

    fixed security issues

--- backend/models.py
+++ backend/models.py
@@ -2,10 +2,11 @@ import os
 from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
+from settings import DB_NAME, DB_USER, DB_PASSWORD
 
-database_name = 'trivia'
-database_user = 'test_brain001'
-database_password = 'mosobalaje'
+database_name = DB_NAME
+database_user = DB_USER
+database_password = DB_PASSWORD
 database_path = "postgresql://{}:{}@{}/{}".format(
     database_user, database_password, "localhost:5432", database_name
 )
